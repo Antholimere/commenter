10b1ddb651bfd43b2e6e1e2845ba7ce9c0006df8
Anthony Limere <anthonylimere@gmail.com>
2017-08-17 11:51:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Commenter app
+
+## How to install
+Clone this repository and run:
+```
+npm install
+```
+## How to start
+```
+npm start
+```
+and open http://localhost:8080/ in your browser.
+
+## Test
+
+TBA.
---
+++ test/test_helper.js
@@ -0,0 +1,7 @@
+// Set up testing environment to run like a browser in the command line
+
+// Build 'renderComponent' helper that should render a given react class
+
+// Build helper for simulating events
+
+// Set up chai-jquery
